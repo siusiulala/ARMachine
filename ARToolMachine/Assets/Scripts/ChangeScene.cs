48d2ee45d00659efd8fc8d1f4010cada6ecb72fe
kbala <kbala@siusiulala-2.local>
2016-05-18 00:57:43 +0800

    rework

---
+++ ARToolMachine/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ToAR()
+	{
+		SceneManager.LoadScene ("ARview");
+	}
+
+	public void ToMain()
+	{
+		SceneManager.LoadScene ("explode");
+	}
+
+	public void ToTail()
+	{
+		SceneManager.LoadScene ("tail_all");
+	}
+}
---
+++ ARToolMachine/Assets/Scripts/ChangeVisible.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeVisible : MonoBehaviour {
+
+	public GameObject target;
+	public bool isVisable = false;
+	public void Show()
+	{
+		NGUITools.SetActive(target, true);
+	}
+
+	public void Hide()
+	{
+		NGUITools.SetActive(target, false);
+	}
+
+	public void Switch()
+	{
+		if (isVisable) {
+			NGUITools.SetActive(target, false);
+			isVisable = false;
+		} else {
+			NGUITools.SetActive(target, true);
+			isVisable = true;
+		}
+	}
+}
