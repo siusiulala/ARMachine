86c9e9ef4867b00ffb9eda202339c63170b6cedf
kbala <kbala@siusiulala-2.local>
2016-07-19 17:30:29 +0800

    Feature&Specifications

--- ARToolMachine/Assets/Scripts/ObjectHighlight.cs
+++ ARToolMachine/Assets/Scripts/ObjectHighlight.cs
@@ -7,7 +7,11 @@ public class ObjectHighlight : MonoBehaviour {
 	public string objectNumber;
 	public string objectName;
 	public string objectQuantity;
+	public string[] objectFeature;
 
+	private int boxWidth2 = 0;
+	private int boxHeight2 = 0;
+	private string tmpStr = "";
 
 	private Color startColor;
 	private bool _displayObjectName = false;
@@ -15,6 +19,13 @@ public class ObjectHighlight : MonoBehaviour {
 
 	void Start()
 	{
+		
+		foreach (string fetureStr in objectFeature) {
+			tmpStr += "\n" + fetureStr;
+			boxWidth2 = (fetureStr.Length > boxWidth2) ? fetureStr.Length : boxWidth2;
+		}
+		boxWidth2 = boxWidth2 * 16;
+		boxHeight2 = 90 + objectFeature.Length * 25;
 	}
 
 	void Update(){
@@ -44,13 +55,23 @@ public class ObjectHighlight : MonoBehaviour {
 	{
 		GUIStyle myStyle = new GUIStyle(GUI.skin.button);
 		myStyle.fontSize = 30;
+
+
+
 		if (_displayObjectName) {
 			int boxWidth = (objectNumber.Length > objectName.Length) ? 180 + objectNumber.Length * 25 : 180 + objectName.Length * 25;
+//			int boxHeight = 120 + objectFeature * 25;
 			GUI.Box (new Rect (Event.current.mousePosition.x-75, Event.current.mousePosition.y-140, boxWidth, 120),
 				//"氣球號："+objectIndex+"\n"+
-				"零件號碼："+objectNumber+"\n"+
-				"零件名稱："+objectName+"\n"+
-				"數量："+objectQuantity,myStyle);
+				"零件號碼：" + objectNumber + "\n" +
+				"零件名稱：" + objectName + "\n" +
+				"數量：" + objectQuantity
+				,myStyle);
+			if (objectFeature.Length > 0) {
+				GUI.Box (new Rect (Event.current.mousePosition.x - 75, Event.current.mousePosition.y + 100, boxWidth2, boxHeight2),
+				//"氣球號："+objectIndex+"\n"+
+					tmpStr, myStyle);
+			}
 		}
 	}
 }
